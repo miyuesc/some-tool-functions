941895b527a3a4abef3dba5aa7db69fb74a67b17
MiyueSC <913784771@qq.com>
2022-04-08 15:01:47 +0800

    Update logger.js

--- functions/logger.js
+++ functions/logger.js
@@ -1,95 +1,108 @@
-class Log {
-    static type = ["primary", "success", "warn", "error", "info"];
-    static instance = null;
+function Log() {}
 
-    static typeColor(type = "default") {
-        let color = "";
-        switch (type) {
-            case "primary":
-                color = "#2d8cf0";
-                break;
-            case "success":
-                color = "#19be6b";
-                break;
-            case "info":
-                color = "#909399";
-                break;
-            case "warn":
-                color = "#ff9900";
-                break;
-            case "error":
-                color = "#f03f14";
-                break;
-            case "default":
-                color = "#35495E";
-                break;
-            default:
-                color = type;
-                break;
-        }
-        return color;
+Log.prototype.type = ["primary", "success", "warn", "error", "info"];
+
+Log.prototype.typeColor = function (type) {
+    let color = "";
+    switch (type) {
+        case "primary":
+            color = "#2d8cf0";
+            break;
+        case "success":
+            color = "#19be6b";
+            break;
+        case "info":
+            color = "#909399";
+            break;
+        case "warn":
+            color = "#ff9900";
+            break;
+        case "error":
+            color = "#f03f14";
+            break;
+        default:
+            color = "#35495E";
+            break;
     }
+    return color;
+};
 
-    static print(text, type = "default", back = false) {
-        if (typeof text === "object") {
-            // 如果是對象則調用打印對象方式
-            console.dir(text);
-            return;
-        }
-        if (back) {
-            // 如果是打印帶背景圖的
-            console.log(`%c ${text} `, `background:${this.typeColor(type)}; padding: 2px; border-radius: 4px; color: #fff;`);
-        } else {
-            console.log(
-                `%c ${text} `,
-                `border: 1px solid ${this.typeColor(type)};
+Log.prototype.isArray = function (obj) {
+    return Object.prototype.toString.call(obj) === "[object Array]";
+};
+
+Log.prototype.print = function (text, type = "default", back = false) {
+    if (typeof text === "object") {
+        // 如果是對象則調用打印對象方式
+        this.isArray(text) ? console.table(text) : console.dir(text);
+        return;
+    }
+    if (back) {
+        // 如果是打印帶背景圖的
+        console.log(`%c ${text} `, `background:${this.typeColor(type)}; padding: 2px; border-radius: 4px; color: #fff;`);
+    } else {
+        console.log(
+            `%c ${text} `,
+            `border: 1px solid ${this.typeColor(type)};
         padding: 2px; border-radius: 4px;
         color: ${this.typeColor(type)};`
-            );
-        }
+        );
     }
+};
 
-    static pretty(type = "primary", title, ...text) {
-        if (text.length <= 1) {
-            const info = text[0];
-            if (typeof info === "object") {
-                console.group("Console Group", title);
-                console.log(
-                    `%c ${title}`,
-                    `background:${this.typeColor(type)};border:1px solid ${this.typeColor(type)};
+Log.prototype.printBack = function (type = "primary", title) {
+    this.print(type, title, true);
+};
+
+Log.prototype.pretty = function (type = "primary", title, text) {
+    if (typeof text === "object") {
+        console.group("Console Group", title);
+        console.log(
+            `%c ${title}`,
+            `background:${this.typeColor(type)};border:1px solid ${this.typeColor(type)};
         padding: 1px; border-radius: 4px; color: #fff;`
-                );
-                console.dir(info);
-                console.groupEnd();
-                return;
-            }
-            console.log(
-                `%c ${title} %c ${info} %c`,
-                `background:${this.typeColor(type)};border:1px solid ${this.typeColor(type)};
+        );
+        this.isArray(text) ? console.table(text) : console.dir(text);
+        console.groupEnd();
+        return;
+    }
+    console.log(
+        `%c ${title} %c ${text} %c`,
+        `background:${this.typeColor(type)};border:1px solid ${this.typeColor(type)};
       padding: 1px; border-radius: 4px 0 0 4px; color: #fff;`,
-                `border:1px solid ${this.typeColor(type)};
+        `border:1px solid ${this.typeColor(type)};
       padding: 1px; border-radius: 0 4px 4px 0; color: ${this.typeColor(type)};`,
-                "background:transparent"
-            );
-        } else {
-            console.group("Console Group", title);
-            console.log(
-                `%c ${title}`,
-                `background:${this.typeColor(type)};border:1px solid ${this.typeColor(type)};
-        padding: 1px; border-radius: 4px; color: #fff;`
-            );
-            text.forEach(info => {
-                console.dir(info);
-            });
-            console.groupEnd();
-        }
-    }
+        "background:transparent"
+    );
+};
 
-    static getInstance() {
-        if (this.instance === null) {
-            this.instance = new Log();
-        }
-        return this.instance;
+Log.prototype.prettyPrimary = function (title, ...text) {
+    text.forEach(t => this.pretty("primary", title, t));
+};
+
+Log.prototype.prettySuccess = function (title, ...text) {
+    text.forEach(t => this.pretty("success", title, t));
+};
+
+Log.prototype.prettyWarn = function (title, ...text) {
+    text.forEach(t => this.pretty("warn", title, t));
+};
+
+Log.prototype.prettyError = function (title, ...text) {
+    text.forEach(t => this.pretty("error", title, t));
+};
+
+Log.prototype.prettyInfo = function (title, ...text) {
+    text.forEach(t => this.pretty("info", title, t));
+};
+
+Log.prototype.getInstance = function () {
+    if (!this.instance) {
+        this.instance = new Log();
     }
-}
-export default Log.getInstance();
+    return this.instance;
+};
+
+const logger = new Log();
+
+export default logger.getInstance();
